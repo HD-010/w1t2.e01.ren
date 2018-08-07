648828af173b2d5eef3abb2c1aae0e7dcf062524
HD-010 <912744609@qq.com>
2018-08-07 22:58:15 +0800

    初始化项目

---
+++ data/common.inc.php
@@ -0,0 +1,12 @@
+<?php
+//数据库连接信息
+$cfg_dbtype = 'mysql';
+$cfg_dbhost = '58.87.79.234';
+$cfg_dbname = 'zbjy';
+$cfg_dbuser = 'zbjy';
+$cfg_dbpwd = 'zbjy123';
+$cfg_dbprefix = 'zbjy_';
+$cfg_db_language = 'utf8';
+
+
+?>
